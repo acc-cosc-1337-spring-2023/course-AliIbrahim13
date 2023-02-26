99ec099e4c259f4b21a67be77c065d20623d7904
AliIbrahim13 <123427744+AliIbrahim13@users.noreply.github.com>
2023-02-26 01:53:32 +0000

    Homework 4

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,34 @@
 //add include statements
 
 //add function(s) code here
+// Function definition for factorial
+int factorial(int num)
+{
+    int result = 1;
+    
+    while (num > 0)
+    {
+        result *= num;
+        num--;
+    }
+    
+    return result;
+}
+
+// Function definition for greatest common divisor
+int gcd(int num1, int num2)
+{
+    while (num1 != num2)
+    {
+        if (num1 > num2)
+        {
+            num1 -= num2;
+        }
+        else
+        {
+            num2 -= num1;
+        }
+    }
+
+    return num1;
+}
--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -3,3 +3,9 @@
 /*
 Write prototype for function(s)
 */
+// Function prototype for factorial
+int factorial(int num);
+
+// Function prototype for greatest common divisor
+int gcd(int num1, int num2);
+
