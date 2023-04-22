4421eb3a4a43318014d5cfb51acfc41e04c49a2a
AliIbrahim13 <123427744+AliIbrahim13@users.noreply.github.com>
2023-04-22 22:35:32 +0000

    hw 3 TEST CASE

--- test/homework/03_decisions/03_decisions_tests.cpp
+++ test/homework/03_decisions/03_decisions_tests.cpp
@@ -5,35 +5,22 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+
 #include "decisions.h"
-#include "gtest/gtest.h"
+#include "catch.hpp"
 
-// Test get_letter_grade_using_if function
-TEST(GetLetterGradeUsingIfTest, HandlesValidInput) {
-    REQUIRE(get_letter_grade_using_if(95), "A");
-    REQUIRE(get_letter_grade_using_if(85), "B");
-    REQUIRE(get_letter_grade_using_if(75), "C");
-    REQUIRE(get_letter_grade_using_if(65), "D");
-    REQUIRE(get_letter_grade_using_if(50), "F");
+TEST_CASE("Test get_letter_grade_using_if function") {
+    REQUIRE(get_letter_grade_using_if(95) == "A");
+    REQUIRE(get_letter_grade_using_if(85) == "B");
+    REQUIRE(get_letter_grade_using_if(75) == "C");
+    REQUIRE(get_letter_grade_using_if(65) == "D");
+    REQUIRE(get_letter_grade_using_if(50) == "F");
 }
 
-TEST(GetLetterGradeUsingIfTest, HandlesInvalidInput) {
-    REQUIRE(get_letter_grade_using_if(-5), "Out of range");
-    REQUIRE(get_letter_grade_using_if(105), "Out of range");
-}
-
-// Test get_letter_grade_using_switch function
-TEST(GetLetterGradeUsingSwitchTest, HandlesValidInput) 
-{
-    REQUIRE(get_letter_grade_using_switch(95), "A");
-    REQUIRE(get_letter_grade_using_switch(85), "B");
-    REQUIRE(get_letter_grade_using_switch(75), "C");
-    REQUIRE(get_letter_grade_using_switch(65), "D");
-    REQUIRE(get_letter_grade_using_switch(50), "F");
-}
-
-TEST(GetLetterGradeUsingSwitchTest, HandlesInvalidInput)
-{
-    REQUIRE(get_letter_grade_using_switch(-5), "Out of range");
-    REQUIRE(get_letter_grade_using_switch(105), "Out of range");
+TEST_CASE("Test get_letter_grade_using_switch function") {
+    REQUIRE(get_letter_grade_using_switch(95) == "A");
+    REQUIRE(get_letter_grade_using_switch(85) == "B");
+    REQUIRE(get_letter_grade_using_switch(75) == "C");
+    REQUIRE(get_letter_grade_using_switch(65) == "D");
+    REQUIRE(get_letter_grade_using_switch(50) == "F");
 }
